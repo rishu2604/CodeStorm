8e16b49f9a9476360ba86fe6a92ee94e2fb57fa8
Tamanna Singh <tamanna.singh@kocharsoft.com>
2025-11-28 23:42:43 +0530

    backend structure

--- backend/be/src/auth/auth.controller.spec.ts
+++ backend/be/src/auth/auth.controller.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProductController } from './product.controller';
+import { AuthController } from './auth.controller';
 
-describe('ProductController', () => {
-  let controller: ProductController;
+describe('AuthController', () => {
+  let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [ProductController],
+      controllers: [AuthController],
     }).compile();
 
-    controller = module.get<ProductController>(ProductController);
+    controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
